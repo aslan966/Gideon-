7b848c5c53ae4087dc48a8ed1dc4631ccb55f53e
aslan966 <asekaaslan2014@gmail.com>
2026-08-10 11:58:40 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gideon-cache-v3';
+const CACHE_NAME = 'gideon-cache-v4';
 const APP_SHELL = ['./', './index.html', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
